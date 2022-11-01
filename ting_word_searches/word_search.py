617280bd05187a86ff5fd3effc12673df7a4610b
Antonio Sabino <aslimajr@outlook.com>
2022-10-31 22:12:49 -0300

    Resolução requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = []
+    for i in range(len(instance)):
+        occurrences = []
+        file = instance.search(i)
+        for line, content in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in content.lower():
+                occurrences.append({"linha": line + 1})
+        if occurrences:
+            data.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            })
+    return data
 
 
 def search_by_word(word, instance):
